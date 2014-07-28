884012a81f343f396ca586f60cdbf40ae6d1646a
tcnksm <nsd22843@gmail.com>
2014-07-28 23:32:52 +0900

    Add upload

--- api.go
+++ api.go
@@ -8,12 +8,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 const (
 	RELEASE_URL = "https://api.github.com/repos/%s/%s/releases"
-	UPLOAD_URL  = "https://uploads.github.com/repos/%s/%s/releases/%s/assets"
+	UPLOAD_URL  = "https://uploads.github.com/repos/%s/%s/releases/%d/assets"
 )
 
 type Releases struct {
@@ -28,6 +29,10 @@ type ReleaseRequest struct {
 	Prerelease      bool   `json:"prerelease"`
 }
 
+type ReleaseResponse struct {
+	ID int `json:"id"`
+}
+
 func debugResponseBody(body io.ReadCloser) {
 	if os.Getenv("DEBUG") != "" {
 		body, _ := ioutil.ReadAll(body)
@@ -35,12 +40,61 @@ func debugResponseBody(body io.ReadCloser) {
 	}
 }
 
+func uploadAsset(info Info, path string) error {
+	file, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if file.IsDir() {
+		fmt.Fprintf(os.Stderr, "`%s` is directory, skip it\n", path)
+		return nil
+	}
+
+	v := url.Values{}
+	v.Set("name", file.Name())
+
+	requestURL := fmt.Sprintf(UPLOAD_URL, info.OwnerName, info.RepoName, info.ID) + "?" + v.Encode()
+	debug(requestURL)
+
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", requestURL, f)
+	if err != nil {
+		return err
+	}
+
+	req.ContentLength = file.Size()
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	req.Header.Add("Authorization", fmt.Sprintf("token %s", info.Token))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil
+	}
+	defer res.Body.Close()
+	debug(res.Status)
+
+	if res.StatusCode != http.StatusCreated {
+		if res.StatusCode == 422 {
+			return fmt.Errorf("Github returned %s (this is probably because the release already exists)\n", res.Status)
+		}
+		return fmt.Errorf("Github returned %s\n", res.Status)
+	}
+
+	return nil
+}
+
 func GetReleaseID(info Info) (int, error) {
 
-	url := fmt.Sprintf(RELEASE_URL, info.OwnerName, info.RepoName)
-	debug(url)
+	requestURL := fmt.Sprintf(RELEASE_URL, info.OwnerName, info.RepoName)
+	debug(requestURL)
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
 		return -1, err
 	}
@@ -74,10 +128,10 @@ func GetReleaseID(info Info) (int, error) {
 	return -1, nil
 }
 
-func CreateNewRelease(info Info) error {
+func CreateNewRelease(info Info) (int, error) {
 
-	url := fmt.Sprintf(RELEASE_URL, info.OwnerName, info.RepoName)
-	debug(url)
+	requestURL := fmt.Sprintf(RELEASE_URL, info.OwnerName, info.RepoName)
+	debug(requestURL)
 
 	params := ReleaseRequest{
 		TagName:         info.TagName,
@@ -88,14 +142,14 @@ func CreateNewRelease(info Info) error {
 
 	payload, err := json.Marshal(params)
 	if err != nil {
-		return err
+		return -1, err
 	}
 	debug(string(payload))
 
 	reader := bytes.NewReader(payload)
-	req, err := http.NewRequest("POST", url, reader)
+	req, err := http.NewRequest("POST", requestURL, reader)
 	if err != nil {
-		return err
+		return -1, err
 	}
 
 	req.Header.Add("Content-Type", "application/json")
@@ -104,18 +158,30 @@ func CreateNewRelease(info Info) error {
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return -1, err
 	}
 	defer res.Body.Close()
 
 	debug(res.Status)
-	debugResponseBody(res.Body)
 
 	if res.StatusCode != http.StatusCreated {
 		if res.StatusCode == 422 {
-			return fmt.Errorf("Github returned %s (this is probably because the release already exists)", res.Status)
+			return -1, fmt.Errorf("Github returned %s (this is probably because the release already exists)", res.Status)
 		}
-		return fmt.Errorf("Github returned %s", res.Status)
+		return -1, fmt.Errorf("Github returned %s", res.Status)
 	}
-	return nil
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return -1, err
+	}
+	debug(string(body))
+
+	var releaseResponse ReleaseResponse
+	err = json.Unmarshal(body, &releaseResponse)
+	if err != nil {
+		return -1, err
+	}
+
+	return releaseResponse.ID, nil
 }
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"sync"
 )
 
 type Info struct {
+	ID              int
 	Token           string
 	TagName         string
 	RepoName        string
@@ -52,8 +55,8 @@ func ghrMain() int {
 	tag := os.Args[1]
 	debug(tag)
 
-	artifacts := os.Args[2]
-	debug(artifacts)
+	path := os.Args[2]
+	debug(path)
 
 	// git config --global user.name
 	// tcnksm
@@ -88,24 +91,65 @@ func ghrMain() int {
 		fmt.Fprintf(os.Stderr, err.Error())
 		return 1
 	}
-	debug(id)
 
 	if id == -1 {
-		err = CreateNewRelease(info)
+		id, err = CreateNewRelease(info)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
+			fmt.Fprintf(os.Stderr, err.Error()+"\n")
 			return 1
 		}
 
-		id, err = GetReleaseID(info)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-			return 1
+		if id == -1 {
+			fmt.Fprintf(os.Stderr, "Counld not retrieve release ID\n")
 		}
 	}
-
 	debug(id)
-	//
+	info.ID = id
 
+	file, err := os.Stat(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return 1
+	}
+
+	var files []string
+
+	if file.IsDir() {
+		files, err = filepath.Glob(path + "/*")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+			return 1
+		}
+	} else {
+		files = append(files, path)
+	}
+	debug(files)
+
+	var errorLock sync.Mutex
+	var wg sync.WaitGroup
+	errors := make([]string, 0)
+
+	for _, p := range files {
+		wg.Add(1)
+		go func(p string) {
+			defer wg.Done()
+			fmt.Printf("--> Uploading: %15s\n", p)
+			if err := uploadAsset(info, p); err != nil {
+				errorLock.Lock()
+				defer errorLock.Unlock()
+				errors = append(errors,
+					fmt.Sprintf("%s error: %s", p, err))
+			}
+		}(p)
+	}
+	wg.Wait()
+
+	if len(errors) > 0 {
+		fmt.Fprintf(os.Stderr, "%d errors occurred:\n", len(errors))
+		for _, err := range errors {
+			fmt.Fprintf(os.Stderr, "--> %s\n", err)
+		}
+		return 1
+	}
 	return 0
 }
