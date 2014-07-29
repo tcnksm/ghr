f6d8397175aebfa6680fcf0c24e488214ee1fa73
tcnksm <nsd22843@gmail.com>
2014-07-29 20:47:51 +0900

    Refactoring main

--- main.go
+++ main.go
@@ -8,29 +8,29 @@ import (
 	"sync"
 )
 
-type Info struct {
-	ID              int
-	Token           string
-	TagName         string
-	RepoName        string
-	OwnerName       string
-	TargetCommitish string
-	Draft           bool
-	Prerelease      bool
-}
-
 func debug(v ...interface{}) {
 	if os.Getenv("DEBUG") != "" {
 		log.Println(v...)
 	}
 }
 
-func NewInfo() Info {
-	return Info{
-		TargetCommitish: "master",
-		Draft:           false,
-		Prerelease:      false,
+func artifacts(path string) ([]string, error) {
+	var files []string
+	file, err := os.Stat(path)
+	if err != nil {
+		return nil, err
 	}
+
+	if file.IsDir() {
+		files, err = filepath.Glob(path + "/*")
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		files = append(files, path)
+	}
+
+	return files, nil
 }
 
 func main() {
@@ -53,24 +53,19 @@ func ghrMain() int {
 	}
 
 	tag := os.Args[1]
-	debug(tag)
-
-	path := os.Args[2]
-	debug(path)
+	inputPath := os.Args[2]
 
 	owner, err := GetOwnerName()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		return 1
 	}
-	debug(owner)
 
 	repo, err := GetRepoName()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		return 1
 	}
-	debug(repo)
 
 	info := NewInfo()
 	info.Token = os.Getenv("GITHUB_TOKEN")
@@ -88,7 +83,7 @@ func ghrMain() int {
 	if id == -1 {
 		id, err = CreateNewRelease(info)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error()+"\n")
+			fmt.Fprintf(os.Stderr, err.Error())
 			return 1
 		}
 
@@ -96,45 +91,39 @@ func ghrMain() int {
 			fmt.Fprintf(os.Stderr, "Counld not retrieve release ID\n")
 		}
 	}
-	debug(id)
-	info.ID = id
 
-	file, err := os.Stat(path)
+	info.ID = id
+	debug(id)
+
+	files, err := artifacts(inputPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		return 1
 	}
 
-	var files []string
-
-	if file.IsDir() {
-		files, err = filepath.Glob(path + "/*")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-			return 1
-		}
-	} else {
-		files = append(files, path)
-	}
-	debug(files)
-
 	var errorLock sync.Mutex
 	var wg sync.WaitGroup
 	errors := make([]string, 0)
 
-	for _, p := range files {
+	for _, path := range files {
 		wg.Add(1)
-		go func(p string) {
+		go func(path string) {
 			defer wg.Done()
-			fmt.Printf("--> Uploading: %15s\n", p)
-			// TODO check IsDir
-			if err := UploadAsset(info, p); err != nil {
+
+			f, _ := os.Stat(path)
+			if f.IsDir() {
+				fmt.Fprintf(os.Stderr, "%s is directory, skip it\n", path)
+				return
+			}
+
+			fmt.Printf("--> Uploading: %15s\n", path)
+			if err := UploadAsset(info, path); err != nil {
 				errorLock.Lock()
 				defer errorLock.Unlock()
 				errors = append(errors,
-					fmt.Sprintf("%s error: %s", p, err))
+					fmt.Sprintf("%s error: %s", path, err))
 			}
-		}(p)
+		}(path)
 	}
 	wg.Wait()
 
@@ -145,5 +134,6 @@ func ghrMain() int {
 		}
 		return 1
 	}
+
 	return 0
 }
