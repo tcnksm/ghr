ba0332cc8d4d95bc46e64c7e28f82d8ae39a65a7
tcnksm <nsd22843@gmail.com>
2014-12-16 19:26:20 +0900

    v0.4.0-pre

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const Name = "ghr"
-const Version string = "v0.3.0"
+const Version string = "v0.4.0-pre"
 
 // Describe latest commit hash.
 // This is automatically extracted by git describe --always.
