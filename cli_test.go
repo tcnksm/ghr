d42789b9edae77a66560ab7b857730594b5d941b
tcnksm <nsd22843@gmail.com>
2014-12-17 00:08:32 +0900

    Add tests

---
+++ cli_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRun__versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("ghr --version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("ghr version %s", Version)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_parseError(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("ghr --not-exist", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeParseFlagsError {
+		t.Errorf("expected %d to eq %d", status, ExitCodeParseFlagsError)
+	}
+
+	expected := "flag provided but not defined: --not-exis"
+	if !strings.Contains(errStream.String(), expected) {
+		t.Fatalf("expected %q to contain %q", errStream.String(), expected)
+	}
+}
---
+++ client_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	reset := setEnv("GITHUB_API", "")
+	defer reset()
+
+	exp, _ := url.Parse("https://api.github.com/")
+
+	apiOpts := &GitHubAPIOpts{}
+	setBaseURL(apiOpts)
+
+	client := NewOAuthedClient(apiOpts)
+	if *client.BaseURL != *exp {
+		t.Errorf("expected %q to eq %q", client.BaseURL, exp)
+	}
+}
+
+func TestNewClient_enterprise(t *testing.T) {
+	// Set API endpoint via environmental variable
+	in := "http://github.company.com/api/v3/"
+	reset := setEnv("GITHUB_API", in)
+	defer reset()
+
+	exp, _ := url.Parse(in)
+
+	apiOpts := &GitHubAPIOpts{}
+	setBaseURL(apiOpts)
+
+	client := NewOAuthedClient(apiOpts)
+	if *client.BaseURL != *exp {
+		t.Errorf("expected %q to eq %q", client.BaseURL, exp)
+	}
+}
+
+func TestExtractUploadURL(t *testing.T) {
+
+	in := &GitHubAPIOpts{
+		UploadURL: "https://uploads.github.com/repos/tcnksm/ghr/releases/786857/assets{?name}\"",
+	}
+	exp, _ := url.Parse("https://uploads.github.com/")
+
+	out := ExtractUploadURL(in)
+	if *out != *exp {
+		t.Errorf("expected %q to eq %q", out, exp)
+	}
+}
+
+func TestExtractUploadURL_enterprise(t *testing.T) {
+
+	in := &GitHubAPIOpts{
+		UploadURL: "https://github.company.com/api/v3/repos/tcnksm/ghr/releases/786857/assets{?name}\"",
+	}
+	exp, _ := url.Parse("https://github.company.com/api/v3/")
+
+	out := ExtractUploadURL(in)
+	if *out != *exp {
+		t.Errorf("expected %q to eq %q", out, exp)
+	}
+}
