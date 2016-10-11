2eded96523f46855313c18fb44b1ac095a756df1
tcnksm <nsd22843@gmail.com>
2016-10-11 11:54:05 +0900

    Fix output version

--- version.go
+++ version.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	latest "github.com/tcnksm/go-latest"
+)
+
 // Name is application name
 const Name = "ghr"
 
@@ -9,3 +17,43 @@ const Version string = "v0.5.0"
 // GitCommit describes latest commit hash.
 // This is automatically extracted by git describe --always.
 var GitCommit string
+
+func OutputVersion() string {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "%s version %s", Name, Version)
+	if len(GitCommit) != 0 {
+		fmt.Fprintf(&buf, " (%s)", GitCommit)
+	}
+	fmt.Fprintf(&buf, "\n")
+
+	// Check latest version is release or not.
+	verCheckCh := make(chan *latest.CheckResponse)
+	go func() {
+		fixFunc := latest.DeleteFrontV()
+		githubTag := &latest.GithubTag{
+			Owner:             "tcnksm",
+			Repository:        "ghr",
+			FixVersionStrFunc: fixFunc,
+		}
+
+		res, err := latest.Check(githubTag, fixFunc(Version))
+		if err != nil {
+			// Don't return error
+			Debugf("[ERROR] Check lastet version is failed: %s", err)
+			return
+		}
+		verCheckCh <- res
+	}()
+
+	select {
+	case <-time.After(defaultCheckTimeout):
+	case res := <-verCheckCh:
+		if res.Outdated {
+			fmt.Fprintf(&buf,
+				"Latest version of ghr is %s, please upgrade!",
+				res.Current)
+		}
+	}
+
+	return buf.String()
+}
