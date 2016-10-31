7b984525f621621fe7bcdff30b62bfdbc59b45ca
tcnksm <nsd22843@gmail.com>
2016-10-31 21:25:51 +0900

    Bump up v0.5.3

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ import (
 const Name = "ghr"
 
 // Version is application version
-const Version string = "v0.5.2"
+const Version string = "v0.5.3"
 
 // GitCommit describes latest commit hash.
 // This is automatically extracted by git describe --always.
